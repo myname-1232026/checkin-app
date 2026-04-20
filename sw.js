849b1b34e2142f2ed1d4a2b3d72761649ecc288c
myname-1232026 <3514452037@qq.com>
2026-04-20 20:27:14 +0800

    初始化打卡应用

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'checkin-v1';
+const ASSETS = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
